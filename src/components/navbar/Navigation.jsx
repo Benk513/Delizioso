8b00a0cbcac4635ca07900a7b80498eb75d093ae
Benk513 <79744201+Benk513@users.noreply.github.com>
2024-08-18 17:58:49 +0100

    fix(Navigation):add link functionality to li

--- src/components/navbar/Navigation.jsx
+++ src/components/navbar/Navigation.jsx
@@ -1,11 +1,11 @@
 import { nav } from "@/data/nav"
-
+import { Link } from "react-router-dom";
 const Navigation = () => {
   return (
     <nav>
-      <ul className="flex gap-6 text-primary font-body text-lg">
+      <ul className="flex gap-6 text-primary font-body text-[14px]">
         {nav.map((el) => {
-          return <li key={el.id}> {el.name} </li>;
+          return <li key={el.id}> <Link to={`${el.url}`}>{el.name}</Link></li>;
         })}
       </ul>
        
