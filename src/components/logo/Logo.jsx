a5fd17ea982a59463a2dcf437c7addf2a621e0f9
Benk513 <79744201+Benk513@users.noreply.github.com>
2024-08-18 17:51:39 +0100

    fix(Logo): add link / to logo

--- src/components/logo/Logo.jsx
+++ src/components/logo/Logo.jsx
@@ -1,11 +1,14 @@
  const Logo = () => {
   return (
-      <div className="flex items-center gap-2">
-           
+      <div >
+      <a href="/" className="flex items-center gap-2">
+        
           <div className="px-2 bg-accent rounded-full">
               <h1 className="font-body font-semibold text-accent-foreground text-3xl">D</h1>
           </div>           
           <h1 className="font-body  text-xl font-semibold" >Deliz<span className=" text-accent">ioso</span></h1> 
+      
+      </a>
       
     </div>
   )
