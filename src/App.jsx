38110dfec6c434f431b612ec25ae2adbe28486b7
Benk513 <79744201+Benk513@users.noreply.github.com>
2024-09-29 00:11:15 +0100

    fix(App) : adjust routes

--- src/App.jsx
+++ src/App.jsx
@@ -1,21 +1,31 @@
-import Navbar from './components/navbar/Navbar'
-import Footer from './components/footer/Footer'
-import {Outlet } from 'react-router-dom'
-import useUserStore from './store/useUserStore'
+import {Navigate, Route, Routes } from 'react-router-dom'
+import Layout from './components/layouts/Layout'
+import Login from './pages/login/Login'
+import Home from './pages/home/Home'
+import Reservation from './pages/reservation/Reservation'
+import Cart from './pages/cart/Cart'
+import Menu from './pages/menu/Menu'
+import Contact from './pages/contact/Contact'
+import About from './pages/about/About'
+import Signup from './pages/signup/Signup'
+import Unauthorized from './pages/Unauthorized'
+import Profile from './pages/Profile/Profile'
+import NotFound from './pages/NotFound'
+import RequireAuth from './components/RequireAuth'
 import { useEffect } from 'react'
- function App() {
-
+import useUserStore from './store/useUserStore'
+ 
+function App() {
 
   const initialize = useUserStore((state) => state.initialize);
-
+  const user = useUserStore((state)=>state.user) 
   useEffect(() => {
       // Initialize user data from localStorage
       initialize();
   }, [initialize]);
+  // const user =useUserStore((state) => state.user)
 
-  const user =useUserStore((state) => state.user)
-
-  console.log(user)
+  // console.log(user)
 
   // const {checkAuthentication,checkingAuth, user} = useUserStore()
 
@@ -29,13 +39,43 @@ import { useEffect } from 'react'
  
 
   return (
-    <> <Navbar/>
- 
-      {<Outlet/>}
-    <Footer/>
-    </>
+   <Routes>
+    <Route path="/" element={<Layout/>}>
+    {/* public routes */}
+      <Route index element={<Home />} />
+      <Route path="about" element={<About />} />
+      <Route path="contact" element={<Contact />} />
+      <Route path="menu" element={<Menu />} />
 
-     
+
+
+      <Route path="unauthorized" element={<Unauthorized />} />
+      <Route path="login" element={!user ?<Login /> :<Navigate to='/'/>} />
+      <Route path="signup" element={<Signup />} />
+
+    {/* protected routes */}
+    <Route element={<RequireAuth/>}>
+
+      <Route path="reservation" element={<Reservation />} />
+      <Route path="cart" element={<Cart />} />
+      <Route path="me" element={<Profile />} />
+    </Route>
+
+
+
+    {/* admin only  routes */}
+      {/* <Route path="dashboard" element={<Dashboard />}>
+        
+        <Route path="orders" element={<Orders />} />
+
+      </Route> */}
+      {/* catch all missing routes */}
+      <Route path="*" element={<NotFound />} />
+
+    </Route>
+    
+   </Routes>
+ 
   )
 }
 
--- src/main.jsx
+++ src/main.jsx
@@ -8,6 +8,7 @@ import { BrowserRouter,Route, Routes } from 'react-router-dom'
 
 
 
+
 createRoot(document.getElementById('root')).render(
   <StrictMode>
 
@@ -15,7 +16,8 @@ createRoot(document.getElementById('root')).render(
   <Routes>
     <Route path="/*" element={<App />} />
   </Routes>
-  </BrowserRouter>
+
+   </BrowserRouter>
 
 
       
