044aff9f418aad82d95e7b6e3da36e8e4d02eb49
Benk513 <79744201+Benk513@users.noreply.github.com>
2024-09-28 19:25:02 +0100

    feat(ActiveLinkNavigation)

--- src/components/navbar/Navigation.jsx
+++ src/components/navbar/Navigation.jsx
@@ -1,11 +1,12 @@
 import { nav } from "@/data/nav"
-import { Link } from "react-router-dom";
+import {NavLink, useLocation } from "react-router-dom";
 const Navigation = () => {
+  const location =useLocation()
   return (
     <nav>
       <ul className="flex gap-6 text-primary font-body text-[14px]">
         {nav.map((el) => {
-          return <li key={el.id}> <Link to={`${el.url}`}>{el.name}</Link></li>;
+          return <li key={el.id}> <NavLink to={`${el.url}`} className={`${location.pathname ===el.url ? "text-accent":"" } hover:text-accent transition duration-300`}>{el.name}</NavLink></li>;
         })}
       </ul>
        
