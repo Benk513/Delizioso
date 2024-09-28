f138afbc66f3a55b37263bc90f3033ebafe6870e
Benk513 <79744201+Benk513@users.noreply.github.com>
2024-09-29 00:10:07 +0100

    fix(useUserStore)

--- src/store/useUserStore.js
+++ src/store/useUserStore.js
@@ -1,13 +1,13 @@
 // this is a global user state manager
 import { create } from 'zustand'
 import axios from './../lib/axios'
-import { toast } from 'sonner';
-import {  } from 'react-router-dom';
+
+
 const useUserStore = create((set)=>({
     user: null,
     loading: false,
     checkingAuth:true,
-
+    
     initialize: () => {
         // Check if user data exists in localStorage
         const storedUser = localStorage.getItem('user');
@@ -40,16 +40,17 @@ const useUserStore = create((set)=>({
     login: async ({email,password}) =>{
         set({loading:true})
     
-    try {
-        const res = await axios.post("api/v1/users/login",{email,password})
+        try {
+            const res = await axios.post("api/v1/users/login",{email,password})
         // console.log(res.data)
         console.log(res)
-        toast.success(res.data.message)
+      
         setTimeout(() => {
             set({user:res.data.data.user,loading:false})
             localStorage.setItem('user', JSON.stringify(res.data.data.user));
             
-        }, 1000);        
+        }, 1000);   
+           
         
         //how to access the jwt token from the cookie storage in order to keep the user logged in 
         // const token = res.data.data.token
@@ -62,20 +63,19 @@ const useUserStore = create((set)=>({
 
     } catch (error) {
         if(!error.response){
-            toast.error("Network Error")
+           
             console.log("Netword error")
             set({loading:false})
         }
         else if(error.response.status === 401){
             set({loading:false})
-            toast.error("Invalid Credentials")
+          
             console.log("Invalid Credentials")
             }
             else{
                 set({loading:false})
                 console.log(error.response.data.message)
-                toast.error(error.response.data.message)
-
+             
             }
         
         
